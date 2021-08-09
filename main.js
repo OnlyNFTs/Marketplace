bfb402f936b4a03c457c0f869bb932a6dedec48f
OnlyNFTs <onlynfts@hotmail.com>
2021-08-09 23:40:49 +1000

    Update Commit

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ init = async () => {
     }
  } else {
     alert(3);
-    window.web3 = await Moralis.Web3.enable({provider: 'walletconnect'});
+    window.web3 = await Moralis.Web3.authenticate({provider: 'walletconnect'});
     }
     window.tokenContract = new web3.eth.Contract(tokenContractAbi, TOKEN_CONTRACT_ADDRESS);
     window.marketplaceContract = new web3.eth.Contract(marketplaceContractAbi, MARKETPLACE_CONTRACT_ADDRESS);
