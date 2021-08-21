6b32b46c4543ddf78ef01dab91f60a9d251d2820
OnlyNFTs <onlynfts@hotmail.com>
2021-08-22 00:04:37 +1000

    Update Commit

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { default: BigNumber } = require("bignumber.js");
-
 Moralis.initialize("Yt8nY74340sEhXEWlVCASjPTq5kcBMg4pzqu7iox");
 Moralis.serverURL = 'https://uctux2sj3ina.moralisweb3.com:2053/server';
 
