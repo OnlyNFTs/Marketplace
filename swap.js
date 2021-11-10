7f73112583a98087657f3cca0b73f42522a39ee3
OnlyNFTs <onlynfts@hotmail.com>
2021-11-10 15:49:12 +1000

    Update Commit

--- swap.js
+++ swap.js
@@ -1152,7 +1152,7 @@ getTokenStats = async () => {
         abi: paymentTokenContractAbi 
       };
      // const totalSupply = await Moralis.Web3API.native.runContractFunction(options);
-     const totalSupply = await await Moralis.executeFunction(options);
+     const totalSupply = await Moralis.executeFunction(options);
 
      console.log(totalSupply);
  
