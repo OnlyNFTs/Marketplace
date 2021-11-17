d5d2fde21db71cbb77b3d9d3ff46c62244c47035
OnlyNFTs <onlynfts@hotmail.com>
2021-11-17 17:20:47 +1000

    Update Commit

--- modules/apps/musicplayer.js
+++ modules/apps/musicplayer.js
@@ -26,8 +26,8 @@ loadSong(songs[songIndex])
 //Update song details
 function loadSong(song) {
     title.innerText = song
-    audio.src = `music/${song}.mp3`
-    cover.src = `music/images/${song}.jpg`
+    audio.src = `/music/${song}.mp3`
+    cover.src = `/music/images/${song}.jpg`
 }
 
 function playSong() {
